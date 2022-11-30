eb38b21145e741da7c9ddbe2cb97569ef75fecc8
Thomas Aelbrecht <thomasaelbrecht@live.com>
2022-11-30 15:38:42 +0100

    Fix JWT error response

--- src/createServer.js
+++ src/createServer.js
@@ -133,7 +133,9 @@ module.exports = async function createServer() {
       }
 
       if (ctx.state.jwtOriginalError) {
-        statusCode = 403;
+        statusCode = 401;
+        errorBody.code = 'UNAUTHORIZED';
+        errorBody.message = ctx.state.jwtOriginalError.message;
         errorBody.details.jwtOriginalError = serializeError(ctx.state.jwtOriginalError);
       }
 
